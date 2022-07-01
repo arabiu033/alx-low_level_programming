d76f57e856396b823df66664165befcb0f0a2916
Aminu Rabiu <arabiu033@gmail.com>
2022-07-01 08:06:05 -0700

    ...

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 49; a < 58; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
