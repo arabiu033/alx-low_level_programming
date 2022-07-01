0defff5b174798fd63b8bfd93e436e2553e76339
Aminu Rabiu <arabiu033@gmail.com>
2022-07-01 08:10:53 -0700

    ...

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a > 96; a--)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
