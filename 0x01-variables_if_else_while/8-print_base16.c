d84ce904b8db848fc4842796c11492fc8145d50b
Aminu Rabiu <arabiu033@gmail.com>
2022-07-01 08:17:21 -0700

    ...

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 47; a < 58; a++)
+		putchar(a);
+	for (b = 97; b < 112; b++)
+		putchar(b);
+	putchar('\n');
+
+	return (0);
+}
