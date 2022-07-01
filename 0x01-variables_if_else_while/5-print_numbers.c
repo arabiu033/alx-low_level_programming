b35de528fc3a3a7544b6676282c69a6496526647
Aminu Rabiu <arabiu033@gmail.com>
2022-07-01 08:03:16 -0700

    ...

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+
+	return (0);
+}
