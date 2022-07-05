d608f818783d2e29e002f25a3ff413765a8d653f
Aminu Rabiu <arabiu033@gmail.com>
2022-07-05 09:10:52 -0700

    added new update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ * this function is printing the full alphabets
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i=97; i <= 122; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
