0daa797dfd168141ab2acb7256ef679b3174c789
Aminu Rabiu <arabiu033@gmail.com>
2022-07-05 09:53:14 -0700

    added new update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check for alpha case
+ *
+ * Return: int
+ */
+
+int _islower(int c)
+{
+	int a;
+	
+	if (c >= 97 && c <= 122)
+		a = 1;
+	else
+		a = 0;
+
+	return a;
+}
