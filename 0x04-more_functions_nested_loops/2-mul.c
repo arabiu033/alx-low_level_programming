483ae571fec21be72d77b2aa077d4eb73b877e04
Aminu Rabiu <arabiu033@gmail.com>
2022-07-07 10:07:22 -0700

    added new update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Return: mulltiply number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
