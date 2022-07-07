83f01a5570d948311f8c0ee47a243a896d8dc4f4
Aminu Rabiu <arabiu033@gmail.com>
2022-07-07 04:50:40 -0700

    added new update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ int main(void)
 
 	i = 0;
 /**
-	while (i < 10)
-	{
-		putchar(i);
-	}
-*/
+ * printf - while (i < 10)
+ *	{
+ *		putchar(i);
+ *\o/
+ */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
