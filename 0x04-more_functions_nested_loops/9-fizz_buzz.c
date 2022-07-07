926dbba46e7804791a48dff68ec7a6996d143807
Aminu Rabiu <arabiu033@gmail.com>
2022-07-07 12:28:47 -0700

    added new update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
