6795b3bd067d7031c5ac35b86db8a21ba8b1a488
Aminu Rabiu <arabiu033@gmail.com>
2022-07-12 07:51:31 -0700

    added new update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print a string of characters
+ *
+ * @str: pointer parameter
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
