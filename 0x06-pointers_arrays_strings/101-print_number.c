d3a33f27f18d5d84e9b50fab863eacd4d76fb3a0
Aminu Rabiu <arabiu033@gmail.com>
2022-07-14 07:50:59 -0700

    added new update

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - print a number using characters
+ *
+ * @n: parameter
+ *
+ * Return: void nothing
+ */
+void print_number(int n)
+{
+	unsigned int  holder = 1, dummy = n, x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+		dummy = n;
+	}
+
+	while (dummy > 9)
+	{
+		dummy = dummy / 10;
+		holder = holder * 10;
+	}
+	while (holder != 0)
+	{
+		x = n / holder;
+		n = n % holder;
+		holder = holder / 10;
+		_putchar('0' + x);
+	}
+}
