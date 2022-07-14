cdbfd82c9db742275f08b9e99c5ef4f3730133c3
Aminu Rabiu <arabiu033@gmail.com>
2022-07-14 05:11:23 -0700

    added new update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - append a string2 to string1
+ * @dest: string to make the change on
+ * @src: string to be appended
+ * Return: a pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
