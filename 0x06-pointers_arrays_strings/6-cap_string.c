81fb7d3c3bb281a5a7bec85e91b5a4f96e5fedad
Aminu Rabiu <arabiu033@gmail.com>
2022-07-14 06:34:05 -0700

    added new update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,8 @@ char *cap_string(char *str)
 	{
 		if (i == 0)
 			str[i] = toupper(str[i]);
-		if (str[i] == ' ' || str[i] == '.' || str[i] == '\n')
+		if (str[i] == ' ' || str[i] == '.' || str[i] == '\n' ||
+		    str[i] == '\t')
 			str[i + 1] = toupper(str[i + 1]);
 		i++;
 	}
