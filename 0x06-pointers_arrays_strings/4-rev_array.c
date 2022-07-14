9452d3ba410752307ec2ba365dd944c3fcdc2fd6
Aminu Rabiu <arabiu033@gmail.com>
2022-07-14 05:58:05 -0700

    added new update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "stdio.h"
 
 /**
  * reverse_array - print array in reverse fashion
@@ -9,17 +8,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, temp;
 
-	i = n - 1;
-	while (i >= 0)
+	for (i = 0; i < n / 2; i++)
 	{
-		if (i != n - 1)
-		{
-			printf(", ");
-		}
-		printf("%d", a[i]);
-		i--;
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
-	printf("\n");
 }
