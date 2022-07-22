9b80f28e94efcb26e4453eee94677cd17995df12
Aminu Rabiu <arabiu033@gmail.com>
2022-07-22 14:17:00 -0700

    added new update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,70 +1,38 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-int count(int, int);
 /**
- * main - calculate number of cents to pay
- * @argc: arguments counts
- * @argv: arguments passed
- * Return: 0 succe
+ *main-prints the minimum number of coins
+ *to make change for an amount of money.
+ *@argc:number of commandline arguments.
+ *@argv:pointer to an array of commmand line arguments.
+ *Return:0-success, non-zero-fail.
  */
+
 int main(int argc, char *argv[])
 {
-	int c;
-
 	if (argc == 2)
 	{
-		if (atoi(argv[1]) <= 0)
-		{
-			printf("0\n");
-			return (0);
-		}
+		int i, leastcents = 0, money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
 
-		c = count(0, atoi(argv[1]));
-		printf("%d\n", c);
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				leastcents += money / cents[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", leastcents);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	return (0);
-}
-
-/**
- * count - check for number of cents
- * @a: cents tracker
- * @val: changes left
- * Return: a, cents
- */
-int count(int a, int val)
-{
-	if (val > 25)
-	{
-		a += val / 25;
-		return (count(a, val % 25));
-	}
-	else if (val > 10)
-	{
-		a += val / 10;
-		return (count(a, val % 10));
-	}
-	else if (val > 5)
-	{
-		a += val / 5;
-		return (count(a, val % 5));
-	}
-	else if (val > 2)
-	{
-		a += val / 2;
-		return (count(a, val % 2));
-	}
-	else
-	{
-		a += val;
-	}
-
-	return (a);
 }
