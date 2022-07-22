cbb55a4c8d555de69f2e5ef394e4366153513f84
Aminu Rabiu <arabiu033@gmail.com>
2022-07-22 07:36:09 -0700

    added new update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program to multiply two arguments passed
+ * @argc: arguments count parameter
+ * @argv: string of arguments passed
+ * Return: (0) success
+ */
+int main(int argc, char *argv[])
+{
+	long int result;
+
+	if (argc > 1 && atoi(argv[1]) && atoi(argv[2]))
+	{
+		result = atol(argv[1]) * atol(argv[2]);
+		printf("%ld\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
