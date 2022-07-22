64906aaeeb387795354175ada40c417d5a5474d4
Aminu Rabiu <arabiu033@gmail.com>
2022-07-22 12:43:56 -0700

    added new update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int isDigit(char *s);
 /**
  * main - a program to multiply two arguments passed
  * @argc: arguments count parameter
@@ -10,9 +11,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int result;
+	int result, sig, i;
 
-	if (argc > 1)
+	i = argc;
+	sig = 0;
+	while (--i > 0 && sig != -1)
+		sig = isDigit(*(argv + i));
+	if (argc > 1 && sig == 0)
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
@@ -23,4 +28,23 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+}
+
+
+/**
+ * isDigit - check whether a value is digit
+ * @s: string to inspect
+ * Return: 0 for a number
+ */
+int isDigit(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (!(*(s + i) >= 48 && *(s + i) <= 57))
+			return (-1);
+		++i;
+	}
+	return (0);
 }
