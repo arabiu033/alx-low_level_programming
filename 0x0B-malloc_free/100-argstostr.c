8bb4d9b96f223c19f73799853bb3cc311d18aabb
Aminu Rabiu <arabiu033@gmail.com>
2022-07-25 18:18:31 -0700

    added new update

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int length(char *);
+void copy(char *, char*, int);
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: list of strings
+ * @av: array of strings
+ * Return: string
+ */
+char *argstostr(int ac, char **av)
+{
+	int len, i, l;
+	char *str;
+
+	if (!ac || !av)
+		return (NULL);
+	
+	len = i = 1;
+	while (i < ac)
+		len += length(av[i++]);
+
+	l = 0, i = 1;
+	str = malloc(sizeof(char) * len);
+	if (!str)
+		return (NULL);
+	while (i < ac)
+	{
+		copy(str, av[i], l);
+		l += length(av[i++]) - 1;
+		str[l++] = '\n';
+	}
+
+	return (str);
+}
+
+
+/**
+ * length - return the string length
+ * @str: string to get its length
+ * Return: int length
+ */
+int length(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		++i;
+	return (i + 1);
+}
+
+
+/**
+ * copy - copy a string from one place to another
+ * @str: string to copy;
+ * @str1: where to copy the string to
+ * @i: from
+ * Return: void
+ */
+void copy(char *str, char *str1, int i)
+{
+	int k = 0;
+
+	while (*(str1 + k) != '\0')
+		*(str + i++) = *(str1 + k++);
+}
