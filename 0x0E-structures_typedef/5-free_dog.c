d197b4442f975159328aac06ba8cf84112656d5c
Aminu Rabiu <arabiu033@gmail.com>
2022-08-01 08:13:02 -0700

    added new update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_dog - free dogs
+ * @d: dog to free
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return (NULL);
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
