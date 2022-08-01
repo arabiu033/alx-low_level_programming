f46ac05deedf34c86d9769cf9e2749bf225540f1
Aminu Rabiu <arabiu033@gmail.com>
2022-08-01 07:33:25 -0700

    added new update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,6 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-	if (!d->age)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 }
