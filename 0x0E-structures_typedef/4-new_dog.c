b8ddbfd4981eacc86ca38dbcb3d38a30768bc518
Aminu Rabiu <arabiu033@gmail.com>
2022-08-01 08:03:26 -0700

    added new update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,7 +26,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	d->owner = malloc(sizeof(char) * len(owner));
-	if(!d->owner)
+	if (!d->owner)
 	{
 		free(d->name);
 		free(d);
