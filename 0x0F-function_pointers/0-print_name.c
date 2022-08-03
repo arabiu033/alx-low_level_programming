6284b0f5d82e82a23ab6628435cc995517795875
Aminu Rabiu <arabiu033@gmail.com>
2022-08-03 06:35:18 -0700

    added new update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - fuction that accepts a function pointer and name to print
+ * @name: the name to print
+ * @f: the fuction pointer
+ * Return: void - nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef HEADER
+#define HEADER
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
