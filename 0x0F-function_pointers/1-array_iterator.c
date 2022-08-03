14ae04a05c00c7a8ec163161ad4ba1d6bc2ba956
Aminu Rabiu <arabiu033@gmail.com>
2022-08-03 06:53:21 -0700

    added new update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate through calling the right fuctions
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: the function to call for the iteration
+ * Return: void - nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		(*action)(*(array + i));
+}
