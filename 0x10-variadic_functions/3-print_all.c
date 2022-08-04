894c8e28f28bbed67c8b4026f16e06ca061f1f18
Aminu Rabiu <arabiu033@gmail.com>
2022-08-04 12:20:20 -0700

    added new update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,7 +30,7 @@ void print_all(const char * const format, ...)
 		{
 		case 's':
 			s = va_arg(arr, char *);
-			printf("%s", s ? s : "(nul)");
+			printf("%s", s ? s : "(nil)");
 			break;
 		case 'i':
 			printf("%d", va_arg(arr, int));
