bbdf9ebd6a3316abe71ffefdef2fdbe5902ab403
Aminu Rabiu <arabiu033@gmail.com>
2022-08-04 12:15:20 -0700

    added new update

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+#include "variadic_functions.h"
+
+void dummy(void);
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: void - nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arr;
+	int count = 0, i = 0, sig = 1;
+	char p;
+	char *s;
+
+	va_start(arr, format);
+	while (format[i] != '\0')
+	{
+		p = format[i];
+		if (p == 's' || p == 'c' || p == 'f' || p == 'i')
+			++count;
+		++i;
+	}
+
+	i = 0;
+	while (format[i] != '\0')
+	{
+		--count;
+		switch (format[i])
+		{
+		case 's':
+			s = va_arg(arr, char *);
+			printf("%s", s ? s : "(nil)");
+			break;
+		case 'i':
+			printf("%d", va_arg(arr, int));
+			break;
+		case 'c':
+			printf("%c", '\0' + va_arg(arr, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(arr, double));
+			break;
+		default:
+			sig = 0;
+			++count;
+		}
+		++i;
+		if (count && sig)
+			printf(", ");
+		sig = 1;
+	}
+	printf("\n");
+}
+
+/**
+ * dummy - a dummy fuction
+ * Return: void
+ */
+void dummy(void)
+{
+}
