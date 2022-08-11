cae89be82d391450098dc479553791d0ad708991
Aminu Rabiu <arabiu033@gmail.com>
2022-08-11 15:28:46 -0700

    added new update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pre_main - prints its message befor main
+ * Return: void - nothing
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must %s",
+	       "allow,\nI bore my house upon my back!\n");
+}
