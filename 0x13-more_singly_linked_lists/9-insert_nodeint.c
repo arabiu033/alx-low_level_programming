1d19eacd6ee270e44300d1d5f87b9c3ee64c157d
Aminu Rabiu <arabiu033@gmail.com>
2022-08-15 17:11:40 -0700

    added new update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!(idx - i))
 	{
 		temp->n = n;
-		temp->next = next;
+		temp->next = NULL;
 		prev->next = temp;
 		return (temp);
 	}
