599bd977b643b3414ef29727a4eb63beafcd53ca
arabiu033 <arabiu033@gmail.com>
2022-09-29 14:29:35 -0700

    Adjust

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -17,7 +17,7 @@ size_t dlistint_len(const dlistint_t *h)
 	tranverse->next = h->next;
 	tranverse->prev = h->prev;
 	holder = tranverse;
-
+	count = 0;
 	while (tranverse->prev)
 		tranverse = tranverse->prev;
 
