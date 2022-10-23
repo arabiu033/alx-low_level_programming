2bc3fd5b2d848c59e03ffb5a405c8e656406c985
arabiu033 <arabiu033@gmail.com>
2022-10-23 15:17:05 -0700

    2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * key_index( - return the key index where the array should be store
+ * key_index - return the key index where the array should be store
  * @key: key to work on
  * @size: size of the hash table
  * Return: key index
