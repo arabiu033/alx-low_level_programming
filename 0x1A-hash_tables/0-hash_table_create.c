e3fed37e8a8cffca6fbe4e3633e40a528dc29261
arabiu033 <arabiu033@gmail.com>
2022-10-23 16:11:21 -0700

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash = (hash_table_t *)malloc(sizeof(hash_table_t));
-	int i;
+	unsigned long int i;
 
 	if (!hash)
 		return (NULL);
