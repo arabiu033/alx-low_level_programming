bf37a78b4373c0fb68b8a92934027108b157b10a
arabiu033 <arabiu033@gmail.com>
2022-10-23 16:14:32 -0700

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,7 +5,7 @@
  * @ht: the hash table to add the node to
  * @key: the key to determine the index to add the value
  * @value: the value to add
- * Return: 0 - success
+ * Return: 1 - success
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 
 	if (!new_node)
-		return (1);
+		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 
@@ -24,10 +24,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node->next = NULL;
 		ht->array[index] = new_node;
-		return (0);
+		return (1);
 	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	return (0);
+	return (1);
 }
